f7ee357ca1671cabf2d934f336f72974f4f26881
tencentcloudapi <tencentcloudapi@tencent.com>
2025-12-24 04:13:55 +0800

    release 3.0.1509

--- src/TencentCloud/Monitor/V20180724/Models/DescribeClusterAgentCreatingProgressResponse.php
+++ src/TencentCloud/Monitor/V20180724/Models/DescribeClusterAgentCreatingProgressResponse.php
@@ -20,17 +20,25 @@ use TencentCloud\Common\AbstractModel;
 /**
  * DescribeClusterAgentCreatingProgress返回参数结构体
  *
+ * @method array getResponse() 获取绑定状态response
+ * @method void setResponse(array $Response) 设置绑定状态response
  * @method string getRequestId() 获取唯一请求 ID，由服务端生成，每次请求都会返回（若请求因其他原因未能抵达服务端，则该次请求不会获得 RequestId）。定位问题时需要提供该次请求的 RequestId。
  * @method void setRequestId(string $RequestId) 设置唯一请求 ID，由服务端生成，每次请求都会返回（若请求因其他原因未能抵达服务端，则该次请求不会获得 RequestId）。定位问题时需要提供该次请求的 RequestId。
  */
 class DescribeClusterAgentCreatingProgressResponse extends AbstractModel
 {
     /**
+     * @var array 绑定状态response
+     */
+    public $Response;
+
+    /**
      * @var string 唯一请求 ID，由服务端生成，每次请求都会返回（若请求因其他原因未能抵达服务端，则该次请求不会获得 RequestId）。定位问题时需要提供该次请求的 RequestId。
      */
     public $RequestId;
 
     /**
+     * @param array $Response 绑定状态response
      * @param string $RequestId 唯一请求 ID，由服务端生成，每次请求都会返回（若请求因其他原因未能抵达服务端，则该次请求不会获得 RequestId）。定位问题时需要提供该次请求的 RequestId。
      */
     function __construct()
@@ -46,6 +54,15 @@ class DescribeClusterAgentCreatingProgressResponse extends AbstractModel
         if ($param === null) {
             return;
         }
+        if (array_key_exists("Response",$param) and $param["Response"] !== null) {
+            $this->Response = [];
+            foreach ($param["Response"] as $key => $value){
+                $obj = new BindProgressResponse();
+                $obj->deserialize($value);
+                array_push($this->Response, $obj);
+            }
+        }
+
         if (array_key_exists("RequestId",$param) and $param["RequestId"] !== null) {
             $this->RequestId = $param["RequestId"];
         }
